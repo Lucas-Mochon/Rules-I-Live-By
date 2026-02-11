bd4c4a0ddc783455c2536aea115a0dc325ffbb6e
gorbayotte <mochonlucas@gmail.com>
2026-02-11 13:05:48 +0100

    cors config

--- backend/src/main/java/com/RulesILiveBy/config/SecurityConfig.java
+++ backend/src/main/java/com/RulesILiveBy/config/SecurityConfig.java
@@ -23,6 +23,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {
+                })
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -33,4 +35,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
